f5b96f64711569f7c783df9be410bff9116deea1
Amrit Shenava <amritshenava@outlook.com>
2019-06-02 21:47:54 -0400

    Update gulpfile.jss

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,23 @@ var gulp = require('gulp');
 var browser-sync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
+
+gulp.task('js', function(){
+
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
 gulp.task('serve', ['sass'], function(){
 
   browserSync.init({
     server : "./src";
   });
 
-  gulp.watch();
-  gulp.watch().on();
+  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+  gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['js', 'serve']);
