5981e4af49975ec763db70c9e1a313f84aca868a
Amrit Shenava <amritshenava@outlook.com>
2019-06-02 21:19:32 -0400

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,15 @@
 var gulp = require('gulp');
 var browser-sync = require('browser-sync').create();
 var sass = require('gulp-sass');
+
+gulp.task('serve', ['sass'], function(){
+
+  browserSync.init({
+    server : "./src";
+  });
+
+  gulp.watch();
+  gulp.watch().on();
+});
+
+gulp.task('default', ['js', 'serve']);
