41547d4734b9403dc8219fafc47cfb2b7cb4139e
Amrit Shenava <amritshenava@outlook.com>
2019-06-02 22:02:52 -0400

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,19 @@ var gulp = require('gulp');
 var browser-sync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
-
-gulp.task('js', function(){
+gulp.task('scss', function(){
 
   return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
         .pipe(sass())
         .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+
+});
+
+gulp.task('js', function(){
+
+  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(gulp.dest("src/js"))
         .pipe(browserSync.stream());
 });
 
