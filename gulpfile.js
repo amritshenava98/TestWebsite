b6556f5590cedd1a51b299662e73374cc4eb58e4
Amrit Shenava <amritshenava@outlook.com>
2019-06-02 21:56:21 -0400

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ gulp.task('js', function(){
   return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
         .pipe(sass())
         .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream(
+        .pipe(browserSync.stream());
 });
 
 gulp.task('serve', ['sass'], function(){
